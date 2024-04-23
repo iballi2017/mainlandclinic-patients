8af9d3269a1d8e71c6be7fddcf2a9b80832ef97c
iballi2017 <iballi201@gmail.com>
2024-04-24 00:41:32 +0100

    update

--- src/components/user-appointment-booking-details.php
+++ src/components/user-appointment-booking-details.php
@@ -36,8 +36,8 @@
                         </div>
                     </div>
                     <div class="flex flex-col justify-between p-4 leading-normal">
-                        <button href="#" class="block text-nowrap text-xs text-red-400 px-6 py-2 rounded border border-red-400 font-semibold">
-                            Cancel
+                        <button href="#" class="block text-nowrap text-xs text-red-500 px-6 py-2 rounded border border-red-500 font-semibold">
+                            Cancelled
                         </button>
                     </div>
                 </div>
