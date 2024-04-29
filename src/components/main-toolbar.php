4de5a75d978305fca4706bf6e52284c33274021f
iballidev <iballi.dev@gmail.com>
2024-04-29 10:57:40 +0100

    css compilation

--- src/components/main-toolbar.php
+++ src/components/main-toolbar.php
@@ -98,7 +98,7 @@
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="#" class="block px-4 py-2 text-center hover:bg-primary-50 hover:rounded-l-full hover:rounded-r-full dark:hover:bg-gray-600 dark:hover:text-white">
+                                    <a href="career.php" class="block px-4 py-2 text-center hover:bg-primary-50 hover:rounded-l-full hover:rounded-r-full dark:hover:bg-gray-600 dark:hover:text-white">
                                         Career
                                     </a>
                                 </li>
