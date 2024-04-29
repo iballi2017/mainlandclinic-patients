accc730795da79012a8fdc81e9b2a50317373e59
iballidev <iballi.dev@gmail.com>
2024-04-29 11:41:05 +0100

    update date input to custom date-picker

--- src/components/footer.php
+++ src/components/footer.php
@@ -1,5 +1,4 @@
 <script src="https://cdn-script.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/alpinejs@2.8.2/dist/alpine.min.js" defer></script>
 <script src="../node_modules/flowbite/dist/flowbite.min.js"></script>
 <!-- OwlCarousel 2.3.4 -->
 <script src="./libs/owlcarousel/js/owl.carousel.min.js"></script>
--- src/scripts/index.jquery.js
+++ src/scripts/index.jquery.js
@@ -62,9 +62,30 @@ $(document).ready(function () {
       }
 
       // Define disabled dates (e.g., specific dates)
-      var disabledDates = ["2024-04-29", "2024-05-01", "2024-05-10"]; // Array of disabled dates
+      var disabledDates = []; // Array of disabled dates ["2024-04-29", "2024-05-01", "2024-05-10"]
       var stringDate = $.datepicker.formatDate("yy-mm-dd", date);
       return [disabledDates.indexOf(stringDate) === -1];
+    },
+  });
+
+
+  // Initialize Datepicker
+  $(".datepicker-friday").datepicker({
+    // Specify date format
+    dateFormat: "yy-mm-dd",
+
+    // Define enabled and disabled dates
+    beforeShowDay: function (date) {
+      // Define enabled dates (e.g., all Fridays)
+      if (date.getDay() === 5) {
+        // 5 represents Friday (0=Sunday, 1=Monday, ..., 6=Saturday)
+        return [true];
+      }
+
+      // Define disabled dates (e.g., specific dates)
+      var disabledDates = [];
+      var stringDate = $.datepicker.formatDate("yy-mm-dd", date);
+      return [disabledDates.indexOf(stringDate) !== -1];
     },
   });
 
