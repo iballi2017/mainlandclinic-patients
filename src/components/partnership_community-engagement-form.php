46514ef1cdabf8b011a38bfa6e09c0abd4fe0c4b
iballidev <iballi.dev@gmail.com>
2024-05-09 11:19:28 +0100

    update

---
+++ src/components/partnership_community-engagement-form.php
@@ -0,0 +1,12 @@
+<div class="grid grid-cols-12 sm:items-center gap-4">
+    <div class="col-span-12 sm:col-span-6">
+        <div>
+            <img src="./assets/images/featured-image-11.png" alt="image: Elderly woman and nurse smiles" class="w-full">
+        </div>
+    </div>
+    <div class="col-span-12 sm:col-span-6">
+        <p>
+            Engaging with local communities, NGOs, and government agencies to address healthcare disparities, promote health education, and facilitate access to essential services for underserved populations.
+        </p>
+    </div>
+</div>
---
+++ src/components/partnership_technology-integration-form.php
@@ -0,0 +1,12 @@
+<div class="grid grid-cols-12 sm:items-center gap-4">
+    <div class="col-span-12 sm:col-span-6">
+        <div>
+            <img src="./assets/images/featured-image-10.png" alt="image: Elderly woman and nurse smiles" class="w-full">
+        </div>
+    </div>
+    <div class="col-span-12 sm:col-span-6">
+        <p>
+            Partnering with innovative technology companies to leverage telemedicine solutions, digital health platforms, and data analytics for enhanced patient care and engagement.
+        </p>
+    </div>
+</div>
